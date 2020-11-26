735fa27bf01f260cbbb13970fbe97cc4a3eef573
Jeffry J. Simon <simon.dev93@gmail.com>
2020-11-26 14:59:30 -0400

    test

--- storage/db/seeds/UserSeeder.php
+++ storage/db/seeds/UserSeeder.php
@@ -21,26 +21,24 @@ class UserSeeder extends AbstractSeed
         $random = new Random();
 
         $data = [
-            [
-                'user_activation_email' => $random->uuid(),
-                'email' => "test_user@test.com",
-                'password' => password_hash('bakatest123567', PASSWORD_DEFAULT),
-                'firstname' => 'test1',
-                'lastname' => 'user',
-                'default_company' => 1,
-                'displayname' => 'anonymouss' . rand(1, 100000),
-                'system_modules_id' => 2,
-                'user_last_login_try' => 0,
-                'created_at' => date('Y-m-d H:i:s'),
-                'registered' => date('Y-m-d H:i:s'),
-                'lastvisit' => date('Y-m-d H:i:s'),
-                'user_active' => 1,
-                'user_level' => 1,
-                'dob' => date('Y-m-d'),
-                'sex' => 'M',
-                'user_login_tries' => 0,
-                'is_deleted' => 0
-            ]
+            'user_activation_email' => $random->uuid(),
+            'email' => "test_user@test.com",
+            'password' => password_hash('bakatest123567', PASSWORD_DEFAULT),
+            'firstname' => 'test1',
+            'lastname' => 'user',
+            'default_company' => 1,
+            'displayname' => 'anonymouss' . rand(1, 100000),
+            'system_modules_id' => 2,
+            'user_last_login_try' => 0,
+            'created_at' => date('Y-m-d H:i:s'),
+            'registered' => date('Y-m-d H:i:s'),
+            'lastvisit' => date('Y-m-d H:i:s'),
+            'user_active' => 1,
+            'user_level' => 1,
+            'dob' => date('Y-m-d'),
+            'sex' => 'M',
+            'user_login_tries' => 0,
+            'is_deleted' => 0
         ];
 
         $table = $this->table('users');
